4be9b04e958787bb808e5e28bf95afbe9ba58c42
Alan Pazetto <alanpazetto@profusion.mobi>
2020-01-30 10:32:22 -0300

    chore(app): initial commit with a bootstrap project

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+module.exports = api => {
+  api.cache(true);
+  return {
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            components: './src/components',
+            locales: './src/locales',
+            routes: './src/routes',
+            styles: './src/styles',
+            assets: './src/assets',
+          },
+          root: './src',
+        },
+      ],
+    ],
+  };
+};
